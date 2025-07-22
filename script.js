b10fdfa06622ca4b953a2b05adc94a95a6ef78e2
lemos999 <lemoaxtoria@gmail.com>
2025-07-22 18:26:17 +0900

    Remove all files

--- script.js
+++
@@ -1,6 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-    // All functions are now in separate files.
-    // This script's only purpose is to initialize the application.
-    initialize();
-});
