8a16a78cc78e9322d0c424b92d01397e9d401d57
lemos999 <lemoaxtoria@gmail.com>
2025-07-23 04:03:30 +0900

    d

--- main.js
+++
@@ -1,6 +0,0 @@
-/* main.js */
-import { initializeAllEventListeners } from './js/events/_globalEvents.js';
-
-document.addEventListener('DOMContentLoaded', function () {
-    initializeAllEventListeners();
-});
