49bba8a53bd1fcf310c97551a2ac6b9596560622
lemos999 <lemoaxtoria@gmail.com>
2025-07-24 04:06:31 +0900

    Add files via upload

---
+++ js_test/module_test.js
@@ -0,0 +1,4 @@
+// js_test/module_test.js
+// This is the module in the sub-folder.
+
+export const message = 'Hello from module_test.js!';
---
+++ main_test.js
@@ -0,0 +1,7 @@
+// main_test.js
+// This script tries to import a module from a sub-folder.
+
+import { message } from './js_test/module_test.js';
+
+console.log(message); // "Hello from module_test.js!"
+console.log('Hello from main_test.js!');
