69e7fad70ceb46f529cb3812ff9c22c37ccb5058
lemos999 <lemoaxtoria@gmail.com>
2025-07-24 04:15:08 +0900

    Add files via upload

---
+++ js_test/module_test.js
@@ -0,0 +1,4 @@
+// js_test/module_test.js
+// 하위 폴더에 위치한 모듈입니다.
+
+export const message = 'Hello from module_test.js!';
---
+++ main_test.js
@@ -0,0 +1,10 @@
+// main_test.js
+// 이 스크립트는 하위 폴더의 모듈을 임포트하려고 시도합니다.
+
+try {
+    const { message } = await import('./js_test/module_test.js');
+    console.log('[SUCCESS] ' + message);
+    console.log('[SUCCESS] Hello from main_test.js!');
+} catch (error) {
+    console.error('[FAIL] Failed to import sub-module. Error:', error);
+}
