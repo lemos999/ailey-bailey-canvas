a5ac8d5fe52679bce017ebe96d5d69ba17080189
lemos999 <lemoaxtoria@gmail.com>
2025-07-24 23:54:38 +0900

    ㄹ

--- js_modules/utils/logger.js
+++
@@ -1,6 +0,0 @@
-// [Node.js Gen] /js_modules/utils/logger.js
-console.log('✅ [LOADED] /js_modules/utils/logger.js');
-export const log = (module, message) => {
-    // The '%c' is now 100% safe from parsing errors.
-    console.log(`%c[${module}]%c ${message}`, 'color: yellow; font-weight: bold;', 'color: unset;');
-};
