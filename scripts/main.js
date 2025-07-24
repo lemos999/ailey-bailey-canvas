01047b7b94ba6e7cfc2ed20ef32658acaddf763b
lemos999 <lemoaxtoria@gmail.com>
2025-07-25 06:43:04 +0900

    ㅇ

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+
+import './script_api_settings.js';
+import './script_chat_app.js';
+import './script_firebase.js';
+import './script_notes_app.js';
+import './script_state.js';
+import './script_ui_helpers.js';
