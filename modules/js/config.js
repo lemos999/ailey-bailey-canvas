ab1f340ac9e7398241fa69b7be017b8f150e503a
lemos999 <lemoaxtoria@gmail.com>
2025-07-25 02:48:56 +0900

    ㄹ

--- modules/js/config.js
+++
@@ -1,3 +0,0 @@
-/* Module: config.js - Contains global constants and configuration. */
-export const appId = 'AileyBailey_Global_Space';
-export const defaultModel = 'gemini-2.5-flash-preview-04-17';
