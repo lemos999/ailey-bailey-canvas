d9871f987502a02f1cd52a35d5a52acf1022df9f
lemos999 <lemoaxtoria@gmail.com>
2025-07-24 19:24:00 +0900

    Add files via upload

---
+++ js_modules/utils/helpers.js
@@ -0,0 +1,7 @@
+﻿// [V3.1] /js_modules/utils/helpers.js
+import { log } from 'https://lemos999.github.io/ailey-bailey-canvas/js_modules/utils/logger.js';
+log('HELPERS', 'Helpers module loaded.');
+
+export function performUtilityTask() {
+    log('HELPERS', 'Utility task executed successfully!');
+}
---
+++ js_modules/utils/logger.js
@@ -0,0 +1,6 @@
+﻿// [V3.1] /js_modules/utils/logger.js
+console.log('✅ [LOADED] /js_modules/utils/logger.js');
+export const log = (module, message) => {
+    // This '%c' caused the error with @"..."@ strings. Now it's safe.
+    console.log(`%c[${module}]%c ${message}`, 'color: yellow; font-weight: bold;', 'color: unset;');
+};
