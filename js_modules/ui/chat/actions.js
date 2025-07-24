bfdeb23596ce9273ed4f40e693754fe6e9b8f9ff
lemos999 <lemoaxtoria@gmail.com>
2025-07-24 19:14:58 +0900

    Add files via upload

---
+++ js_modules/ui/chat/actions.js
@@ -0,0 +1,12 @@
+﻿// [V3] /js_modules/ui/chat/actions.js
+import { appState } from 'https://lemos999.github.io/ailey-bailey-canvas/js_modules/core/state.js';
+import { log } from 'https://lemos999.github.io/ailey-bailey-canvas/js_modules/utils/logger.js';
+log('CHAT', 'Chat Actions module loaded.');
+
+export function toggleChatVisibility(renderCallback) {
+    log('CHAT', 'Executing toggleChatVisibility...');
+    appState.isChatVisible = !appState.isChatVisible;
+    appState.lastAction = 'TOGGLE_CHAT';
+    log('CHAT', Global state 'isChatVisible' changed to: " + appState.isChatVisible);
+    renderCallback(); // 상태 변경 후 렌더링 콜백 실행
+}
---
+++ js_modules/utils/logger.js
@@ -0,0 +1,5 @@
+﻿// [V3] /js_modules/utils/logger.js
+console.log('✅ [LOADED] /js_modules/utils/logger.js');
+export const log = (module, message) => {
+    console.log(%c[]%c , 'color: yellow; font-weight: bold;', 'color: unset;');
+};
