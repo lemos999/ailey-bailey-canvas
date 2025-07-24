0ad5a50bf739e0d2b0278aa4fd6ec085b40e6c26
lemos999 <lemoaxtoria@gmail.com>
2025-07-24 19:23:43 +0900

    ㄹ

--- js_modules/utils/logger.js
+++
@@ -1,5 +0,0 @@
-﻿// [V3] /js_modules/utils/logger.js
-console.log('✅ [LOADED] /js_modules/utils/logger.js');
-export const log = (module, message) => {
-    console.log(%c[]%c , 'color: yellow; font-weight: bold;', 'color: unset;');
-};
