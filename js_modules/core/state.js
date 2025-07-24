6253e061f982f2b9049a4fe19c4e6f6c8a12ce1a
lemos999 <lemoaxtoria@gmail.com>
2025-07-24 21:30:56 +0900

    ㄹ

--- js_modules/core/state.js
+++
@@ -1,9 +0,0 @@
-﻿// [V3.1] /js_modules/core/state.js
-import { log } from 'https://lemos999.github.io/ailey-bailey-canvas/js_modules/utils/logger.js';
-log('STATE', 'State module loaded.');
-
-export const appState = {
-    isChatVisible: false,
-    notesCount: 0,
-    lastAction: null
-};
