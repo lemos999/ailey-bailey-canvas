1eeb661916c76399dc134cafb683706b2999c861
Fewweekslater <lemoaxtoria@gmail.com>
2025-09-26 06:10:43 +0900

    Toggle

--- src/04_features_chat/224_chat_ui_controller.js
+++
@@ -1,5 +0,0 @@
-/* --- Ailey & Bailey Canvas --- */
-// File: 224_chat_ui_controller.js
-// Version: 1.0 (Refactored)
-// Description: Acts as a controller, orchestrating calls to other specialized UI modules.
-// This file does not perform rendering itself but delegates to other modules.
--- src/05_features_notes/350_notes_highlights.js
+++
@@ -1,5 +0,0 @@
-
-/* --- Ailey & Bailey Canvas --- */
-// File: 350_notes_highlights.js
-// Version: 4.0 (DEPRECATED)
-// Description: The highlighting feature has been removed from the application. This file is now empty and will be removed in a future update.
